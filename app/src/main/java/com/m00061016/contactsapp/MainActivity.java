8bd00aee0c7ab89d71825ce9f973e4d86a06f6b5
Cmagana6 <0006106@uca.edu.sv>
2018-04-28 15:58:56 -0600

    implementacion de ViewPagerAdapter

--- app/src/main/java/com/m00061016/contactsapp/MainActivity.java
+++ app/src/main/java/com/m00061016/contactsapp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.m00061016.contactsapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Agregando los fragmentos al adaptador
+
+        adapter.AddFragment(new FragmentCall(),"");
+        adapter.AddFragment(new FragmentContact(),"");
+        adapter.AddFragment(new FragmentFav(),"");
+
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_call);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_group);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_star);
+
+        //Remover las sombras de la barra de accion
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
+
+
     }
 }
