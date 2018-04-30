6694aa1cf074e0a62a1b4596e87c1ed8855b4976
Cmagana6 <0006106@uca.edu.sv>
2018-04-29 23:48:05 -0600

    Implementacion del RecyclerView

---
+++ app/src/main/java/com/m00061016/contactsapp/Contact.java
@@ -0,0 +1,47 @@
+package com.m00061016.contactsapp;
+
+public class Contact {
+
+    private String Name,Phone;
+    private int Photo;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phone, int photo) {
+        Name = name;
+        Phone = phone;
+        Photo = photo;
+    }
+
+    //Getter
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public int getPhoto() {
+        return Photo;
+    }
+
+    //Setter
+
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public void setPhoto(int photo) {
+        Photo = photo;
+    }
+
+}
